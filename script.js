784d34d353f2c12a1af9eaa5635af5d8d205fd02
Sayal Subba <sahilphago23@gmail.com>
2026-08-15 23:59:15 +0545

    adding logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const arr = ["🍎",
+const arr = [
+    "🍎",
     "🍎",
     "🐈",
     "🐈",
@@ -13,7 +14,8 @@ const arr = ["🍎",
     "🌎",
     "🌎",
     "🌈",
-    "🌈"];
+    "🌈"
+];
 
 
 let firstCard = null;
@@ -21,16 +23,28 @@ let secondCard = null;
 let score = 0;
 
 function startGame() {
-    let text = '';
+
     let mainPrt = document.getElementById("mainArr");
     mainPrt.innerHTML = "";
+
+    for (let i = arr.length - 1; i > 0; i--) {
+        const newIndex = Math.floor(Math.random() * (i + 1));
+        const temp = arr[i];
+        arr[i] = arr[newIndex];
+        arr[newIndex] = temp;
+    }
+
     arr.forEach((value) => {
         text = value;
         const displayArr = document.createElement("div");
         displayArr.innerHTML = text;
         mainPrt.appendChild(displayArr);
     })
-}
+};
+
+
+
+
 
 function playGame() {
 
